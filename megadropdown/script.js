a8afaf430cd89c9eb78503f17f0e9675baac10e4
Mohammed A. El-Ansary <a.n.s.a.r.y@hotmail.com>
2015-06-06 17:25:00 +0200

    Sub-menu added

--- megadropdown/script.js
+++ megadropdown/script.js
@@ -3,15 +3,27 @@ $( document ).ready(function() {
 		$( '#dropbtn').toggleClass('active');
 		$( '#dropmenu').toggleClass('is-active');
 	});
-	$('.dropdown-wrapper > .dropdown-main > li > a').hover(function() {
+	$('.dropdown-wrapper > .dropdown-menu > li > a').hover(function() {
 		$(this).addClass('active');
 		}, function() {
 		$(this).removeClass('active');
 	});
-	$('input[type=text]').focusin(function() {
+	$('.dropdown-wrapper > .dropdown-menu > li > a.has-sub-menu').hover(function() {
+		var target = "#" + $(this).attr('data-target');
+		$(target).addClass('is-active');
+		}, function() {
+		var target = "#" + $(this).attr('data-target');
+		if(!$( target ).is(':hover'))
+			$(target).removeClass('is-active');
+	});
+	$('.sub-menu').hover(function() {
+		}, function() {
+		$(this).removeClass('is-active');
+	});
+	$('.dropdown-wrapper > .dropdown-menu > li input[type=text]').focusin(function() {
 		$(this).addClass('active');
 	});
-	$('input[type=text]').focusout(function() {
+	$('.dropdown-wrapper > .dropdown-menu > li input[type=text]').focusout(function() {
 		$(this).removeClass('active');
 	});
 });
