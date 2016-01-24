af6509faf346d64e0615ce1a3c6170d81e8d90b8
MohammedAnsary <a.n.s.a.r.y@hotmail.com>
2016-01-24 19:13:32 +0200

    Added a man view to navigate to projects or pens

--- script2.js
+++ script2.js
@@ -1,6 +1,11 @@
 var arr = [ "A", "N", "S", "A", "R", "Y" ];
 var i = 1;
 var loaded = false;
+var navbar = $('#navbar');
+var content = $('#content');
+var projects = $('#projects');
+var selector = $('#selector');
+var pens = $('#pens');
 window.onload = function() {
 					loaded = true;
 				}
@@ -12,16 +17,18 @@ function again() {
 	setTimeout(function(){ $("#loading").removeClass("load"); setTimeout(function(){ $("#loading").html(arr[i++]); again();}, 1500); }, 1500);
 }
 function letsrocknroll() {
-	var navbar = $('#navbar');
-	var projects = $('#projects');
+	navbar.detach();
+	content.detach();
+	projects.detach();
+	selector.detach();
+	pens.detach();
 	$(".outer .middle .inner").html("");
 	$(".outer").animate({height: '10%', backgroundColor: '#2F2F2F', position: 'fixed', top: '0px'}, 500);
 	$(".right").animate({height: '90%', bottom: '0px'}, 500);
 	setTimeout(function(){ $(".outer").css("position", "fixed"); }, 501);
-	setTimeout(function(){  
-		navbar.detach();
+	setTimeout(function(){
 		$(".outer .middle .inner").append(navbar);
-		navbar.css("display", "block");
+		navbar.addClass('shown');
 		//$('.navbar-name').animate({opacity: '1'}, 300);
 		setTimeout(function(){ $('.navbar-name').animate({opacity: '1'}, 300); }, 0);
 		setTimeout(function(){ $('.me').animate({opacity: '1'}, 300); }, 300);
@@ -29,7 +36,7 @@ function letsrocknroll() {
 		setTimeout(function(){ $('.linkedin').animate({opacity: '1'}, 300); }, 900);
 		setTimeout(function(){ $('.facebook').animate({opacity: '1'}, 300); }, 1200);
 		setTimeout(function(){ $('.twitter').animate({opacity: '1'}, 300); }, 1500);
-		
+
 		// var delay = 300;
 		// $('.navbar .navbar-list ul li').each(function(){
 			// var obj = this;
@@ -37,13 +44,25 @@ function letsrocknroll() {
 			// delay += 300;
 		// });
 	}, 1002);
-	setTimeout(function(){  
-		projects.detach();
-		$(".right").append(projects);
-		projects.css("display", "block");
-		//$('.navbar-name').animate({opacity: '1'}, 300);
+	setTimeout(function(){
+		$(".right").append(content);
+		content.addClass('shown');
+		$(".right-scroller div").append(selector);
+		selector.addClass('shown');
+		$('.navbar-name').animate({opacity: '1'}, 300);
 		setTimeout(function(){ $('.section-header').animate({opacity: '1'}, 300); }, 0);
-		setTimeout(function(){ $('.right-scroller').animate({opacity: '1'}, 300); }, 600);
-		setTimeout(function(){ $('.section-footer').animate({opacity: '1'}, 300); }, 1200);
+		setTimeout(function(){ $('.right-scroller').animate({opacity: '1'}, 300); }, 0);
+		setTimeout(function(){ $('.section-footer').animate({opacity: '1'}, 300); }, 600);
 	}, 2500);
 }
+function hideSelector() {
+	selector.animate({opacity: '0'}, 300, function(){ selector.detach() });
+}
+$( document ).ready(function(){
+	$('.selection-projects').click(function(){
+		hideSelector();
+	});
+	$('.selection-pens').click(function(){
+		hideSelector();
+	});
+});
